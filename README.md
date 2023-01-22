cbd587df78b180d425de52c7be214b19a03fe83e
Martin Quevedo <martinqs@live.com.ar>
2023-01-22 19:34:01 -0300

    readme +

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Calculator for Dolar taxes
 
 Dolar tax calculator for credit card use
+(proyecto viejo retomado para Arg Programa)
--- script.js
+++ script.js
@@ -1,47 +1,39 @@
-
-
-//Custom function
+'strict mode'
+//Function
 function calculateTip() {
-    
-//Store the data of the inputs
-let serviceQuality = document.getElementById("serviceQuality").value;
-let numPeople = document.getElementById("totalPeople").value;
-let billAmount = document.getElementById("billAmount").value;
+  //Monto a calcular de $
+  let monto = document.getElementById("Monto").value;
 
-//Quick validation
-if(billAmount==="" || serviceQuality == 0){
+  //Validation
+  if (monto === "" || serviceQuality == 0) {
     window.alert("Please enter some values to get this baby up and running!");
-    return; // this will prevent the function from continuing
+    return; // To prevent infinite looping
+  }
+
+  //Check to see if this input is empty or less than or equal to 1
+  if (numPeople === "" || numPeople <= 1) {
+    numPeople = 1;
+
+    document.getElementById("each").style.display = "none;";
+  } else {
+    document.getElementById("each").style.display = "block";
+  }
+
+  //Do some math!
+  let total = monto * 0.75;
+  // total = Math.round(total*100) * 0.75;
+  // total = total.toFixed(2);
+
+  //Display the tip!
+  document.getElementById("totalTip").style.display = "block";
+  document.getElementById("tip").innerHTML = total;
 }
-
-//Check to see if this input is empty or less than or equal to 1
-if(numPeople ===""|| numPeople <=1) {
-    numPeople =1;
-
-    document.getElementById("each").style.display = "none;"
-
-} else{
-
-     document.getElementById("each").style.display = "block";
-}
-
-//Do some math!
-let total = billAmount * 0.75;
-// total = Math.round(total*100) * 0.75;
-// total = total.toFixed(2);
-
-//Display the tip!
-document.getElementById("totalTip").style.display = "block";
-document.getElementById("tip").innerHTML = total;
-
-}
-
-
 
 //Hide the tip amount on load
-document.getElementById("totalTip"). style.display ="none";
-document.getElementById("each"). style.display = "none";
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
 
 //Clicking the button calls our custom function
-document.getElementById("calculate").onclick= function(){calculateTip();};
-
+document.getElementById("calculate").onclick = function () {
+  calculateTip();
+};
