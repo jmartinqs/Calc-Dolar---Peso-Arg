31762bfcd41f4238fd553ed72e91e3b5770adeaa
Martin Quevedo <martinqs@live.com.ar>
2023-03-15 00:34:25 -0300

    cambios api , cambios html

--- script.js
+++ script.js
@@ -2,43 +2,33 @@
 
 //Function//
 
-function calculateTip() {
-  //Monto a calcular de $
-  let monto = document.getElementById("Monto").value;
+function convertToPesoWithTax() {
+  const dollarAmount = document.getElementById("Monto").value;
+  const apiUrl = "https://www.dolarsi.com/api/api.php?type=valoresprincipales";
 
-  //Validation to initialize
-  if (monto === "") {
-    window.alert("Ingrese un monto para continuar");
-    return; // To prevent infinite looping
-  }
-
-  //Do some math!
-  let total = monto * 0.75;
-  // total = Math.round(total*100) * 0.75;
-  // total = total.toFixed(2);
-
-  //Display the tip!
-  document.getElementById("totalTip").style.display = "block";
-  document.getElementById("tip").innerHTML = total;
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const usdBuyRate = parseFloat(
+        data.find((casa) => casa.casa.nombre === "Dolar Oficial").casa.compra
+      );
+      const pesoAmount = dollarAmount * (usdBuyRate * 1.75);
+      document.getElementById(
+        "peso-amount"
+      ).innerHTML = `ARS ${pesoAmount.toFixed(2)}`;
+    })
+    .catch((error) => {
+      console.error(error);
+      document.getElementById("peso-amount").innerHTML =
+        "Error: Unable to retrieve exchange rates.";
+    });
 }
 
-//Hide the tip amount on load
+//Esconder luego mostrar resultado
 document.getElementById("totalTip").style.display = "none";
 document.getElementById("each").style.display = "none";
 
-//Clicking the button calls our custom function
+//DOM click que callea la function
 document.getElementById("calculate").onclick = function () {
-  calculateTip();
+  convertToPesoWithTax();
 };
-
-// trying to add some APIs
-
-const usdAmount = 100;
-const exchangeRate = 100;
-const impuesto = 0.75;
-
-const arsAmount = usdAmount * exchangeRate * (1 + impuesto);
-
-console.log(
-  `${usdAmount} USD = ${arsAmount.toFixed(2)} ARS (including 75% tax)`
-);
